69cc651f9b5d30632e0247c3e79f0724950ecf1b
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-03-03 14:43:17 +0000

    Restructure project.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='montecarlino',
-      version='0.1.2',
+      version='0.1.5',
       author='Roberto Agostino Vitillo',
       author_email='rvitillo@mozilla.com',
       description='Parellel implementation of bootstrap confidence intervals and permutation tests.',
